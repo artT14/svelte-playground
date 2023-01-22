8fe7f954cf13c1a535b38c75ebd806049fe64e06
ArtT14 <lamferme@gmail.com>
2023-01-22 03:11:17 -0800

    left off at 22

---
+++ src/AppEventForwarding.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Outer from "./components/Outer.svelte";
+	function handleGreet(event){
+		alert(event.detail)
+	}
+</script>
+
+<main>
+	<Outer on:greet={handleGreet}/>
+</main>
+
+<style>
+	main {
+		/* text-align: center; */
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Inner.svelte
@@ -0,0 +1,9 @@
+<script>
+	import {createEventDispatcher} from 'svelte'
+	import GreetButton from './GreetButton.svelte';
+	const dispatch = createEventDispatcher();
+	function greet(){
+		dispatch("greet","hello")
+	}
+</script>
+<GreetButton on:click={greet}/>
