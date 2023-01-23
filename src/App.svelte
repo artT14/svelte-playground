ffa5c5d243d5e266baed8c12150387a4d9423b53
artT14 <lamferme@gmail.com>
2023-01-22 20:38:27 -0800

    Module Context + Stores intro

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,7 @@
 <script>
-    import Display from "./components/Display.svelte";
-    import Increment from "./components/Increment.svelte";
-    import Decrement from "./components/Decrement.svelte";
-    import Reset from "./components/Reset.svelte";
 </script>
 
 <main>
-    <Display/>
-    <Increment/>
-    <Decrement/>
-    <Reset/>
 </main>
 
 <style>
