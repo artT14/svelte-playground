a250d8a89257d66d74f143a1dd1cadda9a0b71a7
artT14 <lamferme@gmail.com>
2023-01-22 19:08:43 -0800

    slots

---
+++ src/AppSlots.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Card from "./components/Card.svelte";
+</script>
+
+<main>
+	<Card>Card Content 1</Card>
+	<Card><h2>Card Content 2</h2></Card>
+	<Card><img alt src="https://picsum.photos/200"/></Card>
+	<Card/>
+</main>
+
+<style>
+	main {
+		/* text-align: center; */
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Card2.svelte
@@ -0,0 +1,25 @@
+<div class="card">
+    <div class="card-header">
+        <slot name="header"></slot>
+    </div>
+    <div class="card-content">
+        <slot name="content">Default Content</slot>
+    </div>
+    {#if $$slots.footer}<hr/>{/if}
+    <div class="card-footer">
+        <slot name="footer"></slot>
+    </div>
+</div>
+
+<style>
+    .card{
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        padding: 16px;
+        margin-bottom: 20px;
+        width: 200px;
+    }
+    .card:hover{
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+</style>
